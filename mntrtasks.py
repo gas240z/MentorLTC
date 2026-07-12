4fd26c18dbffc9fa771d01b96b66f8a1aa374201
gas240z <isayev.mustafa.1999@gmail.com>
2026-07-12 14:56:50 +0400

    Add files via upload

---
+++ mntrtasks.py
@@ -0,0 +1,70 @@
+#task1
+# mehsul1 = 4.50  
+# mehsul2 = 12.99 
+# mehsul3 = 7.25  
+# umumi = mehsul1 + mehsul2 + mehsul3
+# print("Mebleg", umumi)
+
+#task2
+# year = int(input("Il: "))
+# age = 2026 - year
+# print("Yasiniz:", age)
+
+
+#task3
+# en = float(input("Duzbucaqlının enini daxil edin: "))
+# uzunluq = float(input("Duzbucaqlının uzunlugunu daxil edin: "))
+# sahe = en * uzunluq
+# perimetr = 2 * (en + uzunluq)
+# print("Sahe:", sahe)
+# print("Perimetr:", perimetr)
+
+#task4
+# a = int(input("Birinci ededi daxil edin: "))
+# b = int(input("İkinci ededi daxil edin: "))
+# print("Adi:", a / b)
+# print("Tam:", a // b)
+# print("Qalıq:", a % b)
+
+#task5
+# bal = int(input("İmtahan balınızı daxil edin: "))
+# davamiyyet = int(input("Davamiyyet faizini daxil edin: "))
+# netice = bal >= 60 and davamiyyet >= 80
+# print("Qebul:", netice)
+
+# bal = int(input("İmtahan balınızı daxil edin: "))
+# davamiyyet = int(input("Davamiyyet faizini daxil edin: "))
+# if bal >= 60 and davamiyyet >= 80:
+#     print("Qebul olundu: True")
+# else:
+#     print("Qebul olundu: False")
+
+#task6
+# qiymet = float(input("Qiymet daxil edin: "))
+# endirim = float(input("Endirim faizini daxil edin: "))
+# qiymet -= qiymet * endirim / 100
+# print("Endirimli qiymet:", qiymet)
+# qiymet2 = (qiymet * endirim) / 100
+# qiymet2 = qiymet - qiymet2
+# print("Endirimli qiymet:", qiymet2)
+
+#task7
+# eded = float(input("Eded daxil edin: "))
+# print("Mutleq deyer:", abs(eded))
+# print("Round:", round(eded, 2))
+
+#task8
+# istifadeci_adi = input("İstifadəçi adını daxil edin: ")
+# shifre = input("Şifrəni daxil edin: ")
+
+# giris_ugurlu = istifadeci_adi == "admin" and shifre == "1234"
+# print("Giriş uğurlu:", giris_ugurlu)
+# print("Giriş uğursuz:", not giris_ugurlu)
+
+
+# istifadeci_adi = input("İstifadəçi adını daxil edin: ")
+# shifre = input("Şifrəni daxil edin: ")
+# if istifadeci_adi == "admin" and shifre == "1234":
+#     print("True")
+# else:
+#     print("False")
