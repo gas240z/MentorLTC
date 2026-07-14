99b69a873ad6d2649dd110869b8e480cffa157ea
gas240z <isayev.mustafa.1999@gmail.com>
2026-07-14 10:54:59 +0400

    Add files via upload

---
+++ cycletasks.py
@@ -0,0 +1,44 @@
+# for i in range(1,11):
+#     print(i)
+
+# for i in range(1,11):
+#     if i%2!=0:
+#         print(i)
+
+#
+# for i in range(1,6):
+#     print(i**2)
+
+# meyveler = ["alma",'heyva','nar']
+# for index,i in enumerate(meyveler,start=1):
+#     print(index,i)
+#
+# ededler = [4,7,2,-3,5,-1]
+# for i in ededler:
+#     print(i)
+#     if i <0:
+#         print("menfi eded tapildi, dovr bitdi")
+#         break
+
+
+# for i in range(1,50):
+#     if i%5==0 and i%3==0:
+#         print(i)
+
+# user = int(input("Eded daxil edin:"))
+# for i in range(user+1):
+#     if i%5==0:
+#         print(i)
+
+# qiymet = [2,5,3]
+# cemi_mebleg=0
+# for i in qiymet:
+#     cemi_mebleg+=i
+#     print("Odenliecek mebleg:",cemi_mebleg, 'manat')
+
+# user = int(input("Eded daxil edin:"))
+# cem=0
+# for i in range(user):
+#     if i%2==0:
+#         cem+=i
+# print(cem)
