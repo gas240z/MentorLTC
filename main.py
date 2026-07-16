df500bf76cb40185c3e8de4ac8ce37f31784aefb
gas240z <isayev.mustafa.1999@gmail.com>
2026-07-16 21:07:35 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,91 @@
+#task1
+
+# user = float(input("Speed: "))
+# if user >= 110:
+#     print("Speed is greater than 110")
+# elif 60<=user<=110:
+#     print("Normal speed")
+# elif user<=60:
+#     print("Yavah gedirsiz zolaga cixin ")
+# else:
+#     print("Only float or int NUMBERS")
+
+#task2
+# qiymet = float(input("Mehsulun qiymeti: "))
+# endirim = qiymet*0.15
+# end_qiymet = qiymet - endirim
+# print(f'Mehsulun endirimli qiymeti qiymeti: {end_qiymet}')
+
+
+#task3
+# user = int(input('Eded: '))
+# if user > 0:
+#     print("Eded musbetdir")
+# elif user < 0:
+#     print("Eded negativ")
+# elif user == 0:
+#     print("Eded zero")
+# else:
+#     print('Only numbers allowed')
+
+
+#task4
+# age = int(input("Enter your age: "))
+# if age <-12 or age >= 65:
+#     print("Ticket free")
+# else:
+#     print("Ticket not free, 10AZN")
+
+
+#task5
+# say = int(input('Say:'))
+# if say%2 == 0:
+#     print('even')
+# else:
+#     print('odd')
+
+
+
+#task6
+# a = int(input("a: "))
+# b = int(input("b: "))
+# c = int(input("c: "))
+#
+# if a > b and a> c:
+#     print('a en boyukdur')
+# elif b > a and b> c:
+#     print('b en boyukdur')
+# elif c > a and c> b:
+#     print('c en boyukdur')
+
+
+#task7
+# bal = int(input('Bal daxil edin: '))
+# if bal >= 90:
+#     print('Qiymet A')
+# elif bal >= 75:
+#     print('Qiymet B')
+# elif bal >= 60:
+#     print('Qiymet C')
+# else:
+#     print('Qiymet F, kesildiniz')
+
+
+#task8
+# a = float(input('Birinci eded: '))
+# b = float(input('Ikinci eded eded: '))
+# op = input('+ - * / : ')
+#
+# if op == '+':
+#     print(a + b)
+# elif op == '-':
+#     print(a - b)
+# elif op == '*':
+#     print(a * b)
+# elif op == '/' and b!=0:
+#     print(a / b)
+# else:
+#     print('Error')
+
+
+
