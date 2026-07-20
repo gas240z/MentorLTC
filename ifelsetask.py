eb5fc5e4c0a27e360ddabcf99b3ef9901b2ef224
gas240z <isayev.mustafa.1999@gmail.com>
2026-07-20 15:17:39 +0400

    Add files via upload

---
+++ ifelsetask.py
@@ -0,0 +1,74 @@
+# stekan = int(input("Gunde nece stakan su icirsiniz? "))
+
+# if stekan <= 3:
+#     print("Az icirsiniz")
+# elif stekan <= 7:
+#     print("Yaxsidir")
+# else:
+#     print("Ela")
+
+
+# mebleg = float(input("Mebleg daxil edin: "))
+
+# if mebleg < 100:
+#     print(f"Odeniş: {mebleg} AZN")
+# elif mebleg <= 300:
+#     endirim = mebleg * 0.10
+#     yekun = mebleg - endirim
+#     print(f"Odeniş: {yekun} AZN")
+# else:
+#     endirim = mebleg * 0.20
+#     yekun = mebleg - endirim
+#     print(f"Odeniş: {yekun} AZN")
+
+
+# gun = int(input("gunun nomresin daxil edin(1-7): "))
+
+# if gun == 6 or gun == 7:
+#     print("istirahet edin")
+# else:
+#     print("is gunudu")
+
+
+
+# mebleg = float(input("Mebleg: "))
+
+# if mebleg > 50:
+#     print(f"Odeniş: {mebleg} AZN")
+# else:
+#     yekun = mebleg + 3
+#     print(f"Odeniş: {yekun} AZN")
+
+
+# saat = int(input("saatı daxil edin: "))
+
+# if saat >= 22 or saat < 7:
+#     print("Gece rejimi aktiv.")
+# else:
+#     print("Gunduz rejimi aktiv.")
+
+
+# sefer = int(input("Nece defe metro istifade edeceksiniz? "))
+
+# ferdi = 0.30 * sefer
+# abonement = 10.0
+
+# if ferdi < abonement:
+#     ferq = abonement - ferdi
+#     print(f"Ferdi: {round(ferq, 2)} AZN")
+# else:
+#     ferq = ferdi - abonement
+#     print(f"Abonement: {round(ferq, 2)} AZN")
+
+
+# bal = int(input("İmtahan balınızı daxil edin: "))
+# davamiyyet = float(input("Davamiyyet faizinizi daxil edin: "))
+
+# if bal >= 60 and davamiyyet > 80:
+#     print("Sertifikat almağa haqq qazandınız!")
+# elif bal < 60 and davamiyyet <= 80:
+#     print("Həm bal, həm davamiyyət şərti ödənilmədi.")
+# elif bal < 60:
+#     print("İmtahan balınız kifayət deyil.")
+# else:
+#     print("Davamiyyətiniz kifayət deyil.")
